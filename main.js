3e62ce4d5bf4f87eb7efce19e285fd9611ae2679
W1NT3R13 <elit.drogon@gmail.com>
2023-01-27 18:31:30 +0100

    fix: added console log

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-let colorElement
-console.log(colorElement)
+let colorElement;
+console.log(colorElement);
 
-colorElement = "asd"
+colorElement = "asd";
 
-console.log(colorElement)
+console.log(colorElement);
 
-x = "1"
-y = "5"
+x = "1";
+y = "5";
 
-console.log(x - y)
-console.log(x / y)
-console.log(x * y)
-console.log(x + y)
-console.log(+x + +y)
-console.log(parseInt(x) + parseInt(y))
-console.log(y ** 2)
-console.log(y ** 2.85)
+console.log(x - y);
+console.log(x / y);
+console.log(x * y);
+console.log(x + y);
+console.log(+x + +y);
+console.log(parseInt(x) + parseInt(y));
+console.log(y ** 2);
+console.log(y ** 2.85);
+console.log(y ** 4.81);
