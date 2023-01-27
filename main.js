d3c9665f3dac91b1a24f8a019c03d1a50d45325e
W1NT3R13 <elit.drogon@gmail.com>
2023-01-27 18:21:40 +0100

    CSS file cleaned up

---
+++ main.js
@@ -0,0 +1,18 @@
+let colorElement
+console.log(colorElement)
+
+colorElement = "asd"
+
+console.log(colorElement)
+
+x = "1"
+y = "5"
+
+console.log(x - y)
+console.log(x / y)
+console.log(x * y)
+console.log(x + y)
+console.log(+x + +y)
+console.log(parseInt(x) + parseInt(y))
+console.log(y ** 2)
+console.log(y ** 2.85)
