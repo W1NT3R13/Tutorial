9fa86e18fe5cc8c1dc3e3b0ab371e1b7a510d3f3
W1NT3R13 <elit.drogon@gmail.com>
2023-02-03 15:10:59 +0100

    Done HW

--- main.js
+++ main.js
@@ -27,3 +27,14 @@ if ([]) {
 }
 
 // isAlive ? console.log("Yes he is alive") : console.log("You died");
+
+let answer = prompt("Please provide your answer.");
+if (answer === "Blue") {
+  console.log(
+    "wtf bro you based. What are you an NPC? Everyone likes blue don't be a basic bitch. TEAM RED YOU FUCKER!"
+  );
+} else if (answer === "Red") {
+  console.log("LETS FUCKING GOOO TEAM RED");
+} else if (answer === "Yellow") {
+  console.log("Odd choice... That's my fav too good job bro.");
+}
